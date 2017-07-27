704345bc7415662e95bf57306ed9180586e7f93a
RoseLand96 <1716210001@utec-tgo.edu.mx>
2017-07-26 23:15:58 -0500

    Add files via upload

---
+++ Templates/Baseadmin.dwt.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-l" />
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Administration Buy'O</title>
+<link rel="shortcut icon" type="image/x-icon" href="../images/icon.ico">
+<!-- TemplateEndEditable -->
+<link href="../estilo/estilo.css" rel="stylesheet" type="text/css" />
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+
+<body>
+
+<div class="container">
+  <div class="header"><img src="../images/image1.png" width="149" height="90" alt="logo" />
+
+    <!-- end .header --></div>
+  <div class="sidebar1">
+  <?php include("../includes/cabeceraadmin.php");
+  ?>
+  
+   <!-- end .sidebar1 --></div>
+  <div class="content"><!-- TemplateBeginEditable name="EditRegion1" -->EditRegion1<!-- TemplateEndEditable -->
+   
+    
+    <!-- end .content --></div>
+  <div class="footer">
+    <p>Administration Buy'O</p>
+    <!-- end .footer --></div>
+  <!-- end .container --></div>
+</body>
+</html>
---
+++ Templates/principal.dwt.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>BUY'O </title>
+<link rel="shortcut icon" type="image/x-icon" href="images/icon.ico">
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+<link href="../estilo/principal.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+<div class="container">
+  <div class="header">
+    <div class="headerinterior"><a href="../index.php"><img src="../images/image1.png" width="110" height="50" alt="logo" /></a>
+    <?php include("includes/catalogo2.php"); ?>
+    </div>
+    </div>
+     
+  <div class="subcontenedor">
+              <div class="sidebar1">
+              <?php include("includes/catalogo.php"); ?>
+              
+              <!-- end .sidebar1 --></div>
+    <div class="content">
+                <h1><!-- TemplateBeginEditable name="titulo" -->Instrucciones<!-- TemplateEndEditable --></h1>
+                <!-- TemplateBeginEditable name="EditRegion4" -->Contenido<!-- TemplateEndEditable -->
+      <!-- end .content --></div>
+    <!-- end .subcontenedor --></div>
+  <div class="footer">
+    <p>Pied de pagina</p>
+    <!-- end .footer --></div>
+  <!-- end .container --></div>
+</body>
+</html>
