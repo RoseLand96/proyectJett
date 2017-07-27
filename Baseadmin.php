434f247300b2d68f5fc30bdd157590e068f43c5d
RoseLand96 <1716210001@utec-tgo.edu.mx>
2017-07-26 23:18:00 -0500

    Add files via upload

---
+++ Baseadmin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-l" />
+<title>Documento sin título</title>
+<link href="estilo/estilo.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+<div class="container">
+  <div class="header"><a href="#"><img src="" alt="Insertar logotipo aquí" name="Insert_logo" width="180" height="90" id="Insert_logo" style="background-color: #C6D580; display:block;" /></a> 
+    <!-- end .header --></div>
+  <div class="sidebar1">
+  <?php include("includes/cabeceraadmin.php");
+  ?>
+  
+   <!-- end .sidebar1 --></div>
+  <div class="content">
+   
+    
+    <!-- end .content --></div>
+  <div class="footer">
+    <p>Administration Buy'O</p>
+    <!-- end .footer --></div>
+  <!-- end .container --></div>
+</body>
+</html>
