a69f7e4f97b8a38ec1c59a3ac83e5a175fca1365
RoseLand96 <1716210001@utec-tgo.edu.mx>
2017-07-27 01:54:24 -0500

    Delete carrito_tpv.php

--- carrito_tpv.php
+++
@@ -1,7 +0,0 @@
-<?php require_once('Connections/connectionBuyo.php'); ?>
-<?php 
-$total = number_format($_SESSION["Totalcompra"], 2, ".", "");
-$total = $total * 100;
-//"M978" sinifica que el precio es en euros
-echo "M978".$total."\r\n1\r\nBUYO\r\nProductos Buyo\r\n1\r\n".$total;
-?>
