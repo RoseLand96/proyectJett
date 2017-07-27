f77b023d77ddd4ebfe21bfcb6df6269da3c0c732
RoseLand96 <1716210001@utec-tgo.edu.mx>
2017-07-26 23:18:22 -0500

    Add files via upload

---
+++ borrar.php
@@ -0,0 +1,29 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+</head>
+
+<body>
+<table width="100%" border="0" cellspacing="3" cellpadding="3">
+  <tr>
+    <td><img src="images/image1.png" width="177" height="106" longdesc="index.php" /></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td><p>Estimado Cliente:</p>
+      <p>&nbsp;</p>
+    
+  </tr>
+  <tr>
+    <td>Muchas gracias, puede contactarnos a través de nuestro correo electronico<br />
+    <a href="mailto:buyo@yahoo.fr      ">buyo@yahoo.fr </a><br /></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
+
