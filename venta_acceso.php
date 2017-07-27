f489ea245c6b4b6a3576412da5c0b62aced83e0f
RoseLand96 <1716210001@utec-tgo.edu.mx>
2017-07-26 23:23:52 -0500

    Add files via upload

---
+++ venta_acceso.php
@@ -0,0 +1,139 @@
+<?php require_once('Connections/connectionBuyo.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+?>
+<?php
+// *** Validate request to login to this site.
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+$loginFormAction = $_SERVER['PHP_SELF'];
+if (isset($_GET['accesscheck'])) {
+  $_SESSION['PrevUrl'] = $_GET['accesscheck'];
+}
+
+if (isset($_POST['Email'])) {
+  $loginUsername=$_POST['Email'];
+  $password=$_POST['Password'];
+  $MM_fldUserAuthorization = "";
+  $MM_redirectLoginSuccess = "venta_producto.php";
+  $MM_redirectLoginFailed = "acceso_error.php";
+  $MM_redirecttoReferrer = false;
+  mysql_select_db($database_connectionBuyo, $connectionBuyo);
+  
+  $LoginRS__query=sprintf("SELECT ID_usuario, Email, Password FROM usuario WHERE Email=%s AND Password=%s",
+    GetSQLValueString($loginUsername, "text"), GetSQLValueString($password, "text")); 
+   
+  $LoginRS = mysql_query($LoginRS__query, $connectionBuyo) or die(mysql_error());
+  $row_LoginRS = mysql_fetch_assoc($LoginRS);
+  $loginFoundUser = mysql_num_rows($LoginRS);
+  if ($loginFoundUser) {
+     $loginStrGroup = "";
+    
+	if (PHP_VERSION >= 5.1) {session_regenerate_id(true);} else {session_regenerate_id();}
+    //declare two session variables and assign them
+    $_SESSION['MM_Username'] = $loginUsername;
+    $_SESSION['MM_UserGroup'] = $loginStrGroup;	
+	  $_SESSION['MM_ID_usuario'] = $row_LoginRS["ID_usuario"];	      
+
+    if (isset($_SESSION['PrevUrl']) && false) {
+      $MM_redirectLoginSuccess = $_SESSION['PrevUrl'];	
+    }
+    header("Location: " . $MM_redirectLoginSuccess );
+  }
+  else {
+    header("Location: ". $MM_redirectLoginFailed );
+  }
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/principal.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Acceso</title>
+<!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="head" -->
+<script src="SpryAssets/SpryValidationTextField.js" type="text/javascript"></script>
+<link href="SpryAssets/SpryValidationTextField.css" rel="stylesheet" type="text/css" />
+<!-- InstanceEndEditable -->
+<link href="estilo/principal.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+<div class="container">
+  <div class="header">
+    <div class="headerinterior"><a href="index.php"><img src="images/image1.png" width="110" height="50" alt="logo" /></a>
+    <?php include("includes/catalogo2.php"); ?>
+    </div>
+    </div>
+     
+  <div class="subcontenedor">
+              <div class="sidebar1">
+              <?php include("includes/catalogo.php"); ?>
+              
+              <!-- end .sidebar1 --></div>
+    <div class="content">
+                <h1><!-- InstanceBeginEditable name="titulo" --><!-- InstanceEndEditable --></h1>
+                <!-- InstanceBeginEditable name="EditRegion4" -->
+               
+                <form id="form1" name="form1" method="POST" action="<?php echo $loginFormAction; ?>">
+                 <h2>Accede a la Pagina con tus Datos:</h2>
+                  
+                    <span id="sprytextfield1">
+                    <input type="text" placeholder="&#128231;   E-Mail" name="Email" id="textfield2" />
+                    <span class="textfieldRequiredMsg">Se necesita un valor.</span><span class="textfieldInvalidFormatMsg">Formato no válido.</span></span>
+                 
+                     
+                    <span id="sprytextfield2">
+                    <input type="password" placeholder="&#128272;   Password" name="Password" id="textfield3" />
+                    <span class="textfieldRequiredMsg">Se necesita un valor.</span></span>
+                  
+                    <input type="submit" name="button" id="button" value="Enviar" />
+                  
+                </form>
+                <p>&nbsp;</p>
+                <script type="text/javascript">
+var sprytextfield1 = new Spry.Widget.ValidationTextField("sprytextfield1", "email");
+var sprytextfield2 = new Spry.Widget.ValidationTextField("sprytextfield2");
+                </script>
+                <!-- InstanceEndEditable -->
+      <!-- end .content --></div>
+    <!-- end .subcontenedor --></div>
+  <div class="footer">
+    <p>Pied de pagina</p>
+    <!-- end .footer --></div>
+  <!-- end .container --></div>
+</body>
+<!-- InstanceEnd --></html>
